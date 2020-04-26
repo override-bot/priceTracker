494d16d41d70e5d5ac84819f0e13d2bb30f4dab3
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2020-04-26 02:18:59 +0200

    updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+parse3();
 var hours = new Date().getHours();
 
 if (hours < 12) {
@@ -38,11 +39,11 @@ function showData(response) {
     document.getElementById('high').innerHTML = Math.round(response.high_24h).toLocaleString();
 
 }
+showData()
 
 function parse2() {
     var symbol = document.getElementById('symbol').value;
     let link = "https://coinlib.io/api/v1/global?key=7044b78a91d369f3&pref=USD";
-
     let request = new XMLHttpRequest();
     request.open('GET', link);
     request.responseType = 'json';
@@ -63,3 +64,60 @@ function showGlobal(response) {
 
 }
 showGlobal();
+
+function parse3() {
+    let link = "https://api.coinlore.net/api/tickers/";
+    let request = new XMLHttpRequest();
+    request.open('GET', link);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const response = request.response;
+        console.log(response)
+        showTable(response);
+
+    }
+}
+
+function showTable(response) {
+    var tr, td;
+    tbody =
+        document.querySelector('.tbody');
+    tr = tbody.insertRow();
+    td = tr.insertCell();
+
+    td.innerHTML = "COINS";
+    td = tr.insertCell();
+    td.innerHTML = "RANK";
+    td = tr.insertCell();
+    td.innerHTML = "PRICE";
+    td = tr.insertCell();
+    td.innerHTML = "% CHANGE";
+    for (var i = 0; i < (response.data).length; i++) {
+
+        tr = tbody.insertRow(tbody.rows.length);
+        td = tr.insertCell(tr.cells.length);
+        td.innerHTML = response.data[i].name;
+        td = tr.insertCell(tr.cells.length);
+        td.innerHTML = response.data[i].rank;
+        td = tr.insertCell(tr.cells.length);
+        td.innerHTML = response.data[i].price_usd;
+        td = tr.insertCell(tr.cells.length);
+        td.innerHTML = response.data[i].percent_change_1h;
+    }
+}
+showTable();
+
+function openTab(evt, title) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(title).style.display = "block";
+    evt.currentTarget.className += " active";
+}
